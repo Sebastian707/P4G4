aa38b2cef8e1760c881d7bcb4af5a57c4db416b1
99Kajkager <42999933+99Kajkager@users.noreply.github.com>
2026-05-17 14:21:47 +0200

    Updated Tutorial scene:
    - rykket gulvet op
    - opdateret næsten alt teksten undtagen "use dash in air here :D"
    - added materials til alle vægge og gulve
    - resized den collider der viser style meter
    - opdateret modellerne
    - taget pausemenu fra main scene over pause menu for tut scene (men er mostly bare font update)

---
+++ Assets/Scripts/PauseSystem/LoadMainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu : MonoBehaviour
+{
+    public void LoadMainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
